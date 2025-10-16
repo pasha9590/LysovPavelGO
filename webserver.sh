a97caea633dc166d7332d24650a38400fb75ad31
pasha9590 <pasha9590@mail.ru>
2025-10-16 21:28:35 +0300

    16.10

--- webserver.sh
+++ webserver.sh
@@ -2,40 +2,29 @@
 # Простейший веб-сервер на Bash + netcat
 
 # Удаляем старый FIFO (если есть) и создаём новый
-rm -f response
-mkfifo response
+#!/bin/bash
+# Simple Bash Web Server
 
-# Функция для обработки HTTP-запроса
-function handleRequest() {
-  local REQUEST=""
-  local HEADLINE_REGEX='(.*?)\s(.*?)\sHTTP.*?'
+# Директория для временных данных
+WORKDIR="/tmp/webserver"
+RESPONSE_FIFO="$WORKDIR/response"
 
-  # Читаем запрос построчно, пока не встретим пустую строку
+# Подготовка окружения
+mkdir -p "$WORKDIR"
+rm -f "$RESPONSE_FIFO"
+mkfifo "$RESPONSE_FIFO"
+
+handleRequest() {
   while read line; do
-    trline=$(echo "$line" | tr -d '\r\n')
+    echo "$line"
+    trline=$(echo "$line" | tr -d '[\r\n]')
     [ -z "$trline" ] && break
-
-    # Если строка совпадает с первой (headline), извлекаем метод и путь
-    if [[ "$trline" =~ $HEADLINE_REGEX ]]; then
-      REQUEST=$(echo "$trline" | sed -E "s/$HEADLINE_REGEX/\1 \2/")
-    fi
   done
 
-  # Формируем ответ в зависимости от запроса
-  case "$REQUEST" in
-    "GET /")
-      RESPONSE="HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>PONG</h1>"
-      ;;
-    *)
-      RESPONSE="HTTP/1.1 404 NotFound\r\nContent-Type: text/plain\r\n\r\nNot Found"
-      ;;
-  esac
-
-  # Отправляем ответ в FIFO
-  echo -e "$RESPONSE" > response
+  echo -e 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>PONG</h1>' > "$RESPONSE_FIFO"
 }
 
 echo "Listening on port 3000..."
 
-# Запускаем сервер: читаем из FIFO, слушаем порт 3000, обрабатываем запросы
-cat response | nc -lN 3000 | handleRequest
+cd "$WORKDIR" || exit 1
+cat "$RESPONSE_FIFO" | nc -lN 3000 | handleRequest
